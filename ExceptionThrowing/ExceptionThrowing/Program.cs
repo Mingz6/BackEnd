c9cfb87d286cda6224abd62f77df731765c6b22a
Ming Z <MingZ6@outlook.com>
2021-09-27 13:24:09 -0600

    Exception Throwing

---
+++ ExceptionThrowing/ExceptionThrowing/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionThrowing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Exception");
+
+            try
+            {
+                try
+                {
+                    var number = int.Parse("Number123");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Number parsing error", e);
+                    //throw;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine($"Stack Trace: {ex.StackTrace}");
+                Console.WriteLine($"ToString: {ex.ToString()}");
+            }
+
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+    }
+}
