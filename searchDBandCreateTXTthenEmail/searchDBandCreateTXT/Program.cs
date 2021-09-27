0eaf266ac6ea12746cc624a3a58ffe0f5059360c
Ming Z <MingZ6@outlook.com>
2021-09-27 15:32:47 -0600

    Init for new Project - Search DB and Send file

---
+++ searchDBandCreateTXTthenEmail/searchDBandCreateTXT/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace searchDBandCreateTXT
+{
+    class Program
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+
+            logger.Info("job start.");
+            string dbconnectionstring = ConfigurationManager.ConnectionStrings["DBConn"].ConnectionString;
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string dbFile = filePath + @"\Your_Files"+ DateTime.Now.ToString("yyyyMMdd") + ".txt";
+
+            writeFileFromDB(dbconnectionstring, dbFile);
+
+            sendEmail(dbFile);
+
+            logger.Info("job completed.");
+        }
+
+ 
+        public static void writeFileFromDB(string dbConn, string dbFile)
+        {
+            SqlCommand comm = new SqlCommand();
+            comm.Connection = new SqlConnection(dbConn);
+            string sqlQuery = ConfigurationManager.AppSettings["sqlQuery"];
+            comm.CommandText = sqlQuery;
+
+            try
+            {
+                comm.Connection.Open();
+
+                SqlDataReader sqlReader = comm.ExecuteReader();
+
+                using (StreamWriter file = new StreamWriter(dbFile, false))
+                {
+                    while (sqlReader.Read())
+                    {
+                        file.WriteLine(sqlReader["columName"]);
+                    }
+                }
+                sqlReader.Close();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e);
+            }
+            
+            comm.Connection.Close();
+        }
+
+        private static void sendEmail(string dbFile)
+        {
+            try
+            {
+                Outlook.Application oApp = new Outlook.Application();
+                Outlook.MailItem oMsg = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+                
+                oMsg.HTMLBody = ConfigurationManager.AppSettings["Body"]; ;
+                String sDisplayName = "MyAttachment";
+                int iPosition = (int)oMsg.Body.Length + 1;
+                int iAttachType = (int)Outlook.OlAttachmentType.olByValue;
+
+                Outlook.Attachment oAttach = oMsg.Attachments.Add
+                    (@dbFile, iAttachType, iPosition, sDisplayName);
+                //Subject line
+                string subjectLine = ConfigurationManager.AppSettings["Subject"] + DateTime.Now.ToString("yyyyMMdd");
+                oMsg.Subject = subjectLine;
+
+                // Add a recipient.
+                Outlook.Recipients oRecips = (Outlook.Recipients)oMsg.Recipients;
+                
+                List<string> sTORecipsList = new List<string>();
+
+                sTORecipsList.Add(ConfigurationManager.AppSettings["recipient1"]);
+                sTORecipsList.Add(ConfigurationManager.AppSettings["recipient2"]);
+                sTORecipsList.Add(ConfigurationManager.AppSettings["recipient3"]);
+                sTORecipsList.Add(ConfigurationManager.AppSettings["recipient4"]);
+                sTORecipsList.Add(ConfigurationManager.AppSettings["recipient5"]);
+                sTORecipsList.Add(ConfigurationManager.AppSettings["recipient6"]);
+
+                foreach (string t in sTORecipsList)
+                {
+                    Outlook.Recipient oTORecip = oRecips.Add(t);
+                    oTORecip.Type = (int)Outlook.OlMailRecipientType.olTo;
+                    oTORecip.Resolve();
+                }
+                
+                oMsg.Send();
+                oRecips = null;
+                oMsg = null;
+                oApp = null;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+            }
+        }
+    }
+}
---
+++ searchDBandCreateTXTthenEmail/searchDBandCreateTXT/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("searchDBandCreateTXT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("searchDBandCreateTXT")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("55506a51-d61e-488d-8bd1-0de84f876d71")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
